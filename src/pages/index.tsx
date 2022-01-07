811566c3481e6989ed70a5b8b69403fcd99d01ec
Szymon Kolber <szymonkolber@192.168.1.16>
2022-01-07 14:30:55 +0100

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,165 @@
+import MetaMaskButton from "../components/metaMaskButton";
+import { useSdkContext } from "../context/SDKContext";
+import { ContractAddress, toContractAddress } from "@rarible/types";
+import { useEffect, useState, useCallback } from "react";
+import { Maybe } from "../common/maybe";
+import { PrepareMintResponse } from "@rarible/sdk/build/types/nft/mint/domain";
+
 export default function Home() {
+  const { sdk, wallet } = useSdkContext();
+  const [responseObject, setResponseObject] =
+    useState<Maybe<PrepareMintResponse>>(null);
+  const [isMultipleAllowed, setIsMultipleAllowed] =
+    useState<Maybe<boolean>>(null);
+  const [isLazyAllowed, setIsLazyAllowed] = useState<Maybe<boolean>>(null);
+  const [uri, setUri] = useState<string>(
+    "ipfs://QmWLsBu6nS4ovaHbGAXprD1qEssJu4r5taQfB74sCG51tp"
+  );
+  const [supply, setSupply] = useState<string>("1");
+  const [lazy, setLazy] = useState<boolean>(true);
+  const [collectionId, setCollectionId] = useState(
+    "TEZOS:KT18vSGouhJcJZDDgrbBKkdCBjSXJWSbui3i"
+  );
+
+  const prepareMint = async () => {
+    //Contract Address is de facto Union Address
+    //e.g TEZOS:KT18vSGouhJcJZDDgrbBKkdCBjSXJWSbui3i
+
+    let collection;
+
+    try {
+      collection = await sdk.apis.collection.getCollectionById({
+        collection: collectionId,
+      });
+    } catch (err) {
+      console.log("Bad collection address");
+      return;
+    }
+
+    const response = await sdk.nft.mint({ collection });
+
+    setResponseObject(response);
+    setIsMultipleAllowed(response.multiple);
+    setIsLazyAllowed(response.supportsLazyMint);
+  };
+
+  const mint = async () => {
+    try {
+      const mintResponse = await responseObject.submit({
+        uri,
+        lazyMint: lazy,
+        supply: parseInt(supply),
+      });
+      console.log(mintResponse);
+    } catch (err) {
+      console.log(err);
+    }
+    setResponseObject(null);
+  };
+
   return (
-    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
-      <main>
-        <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
+    <div className="container font-mono flex items-center p-4 mx-auto min-h-screen justify-center">
+      <main className="w-2/3 border-2 border-gray-400 p-2">
+        <h1 className=" text-xl code">
+          Rarible Union SDK <span className="text-blue-600">Tezos</span> minting{" "}
         </h1>
+        <br />
+        <br />
+        <span className="text-blue-600">Collection Id:</span>{" "}
+        <input
+          value={collectionId}
+          className="w-2/3"
+          onChange={(e) => {
+            setCollectionId(e.target.value);
+            setResponseObject(null);
+          }}
+          placeholder="Collection Id (ex: ETHEREUM:0xc34c39aa3a83afdd35cb65351710cfc56a85c9f4)"
+        />
+        <br />
+        {responseObject && (
+          <>
+            <span className="text-blue-600">IPFS URI:</span>
+            <Input value={uri} onChange={setUri} placeholder="Metadata URI" />
+            {isMultipleAllowed && (
+              <div>
+                Supply:{" "}
+                <Input
+                  value={supply}
+                  onChange={setSupply}
+                  placeholder="Supply"
+                />
+              </div>
+            )}
+            {isLazyAllowed && (
+              <div>
+                Lazy-mint:{" "}
+                <Checkbox
+                  value={lazy}
+                  onChange={() => setLazy(!lazy)}
+                  label=""
+                />
+              </div>
+            )}
+          </>
+        )}
+        <button
+          className="bg-blue-600 text-indigo-50 py-1 px-3 rounded-md mt-10"
+          onClick={() => {
+            !responseObject ? prepareMint() : mint();
+          }}
+        >
+          {responseObject ? "Mint an NFT" : "Prepare Minting"}
+        </button>
       </main>
     </div>
-  )
+  );
+}
+
+type InputProps = {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+};
+
+export function Input({ value, onChange, placeholder }: InputProps) {
+  const onChangeCallback = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      onChange(e.target.value);
+    },
+    [onChange]
+  );
+
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={onChangeCallback}
+      style={{ width: 500 }}
+      placeholder={placeholder}
+    />
+  );
+}
+
+type CheckboxProps = {
+  value: boolean;
+  onChange: (value: boolean) => void;
+  label: string;
+};
+
+export function Checkbox({ value, onChange, label }: CheckboxProps) {
+  const onChangeCallback = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      onChange(e.target.checked);
+    },
+    [onChange]
+  );
+
+  return (
+    <div>
+      <input type="checkbox" checked={value} onChange={onChangeCallback} />
+      {label}
+    </div>
+  );
 }
